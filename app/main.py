765fcbf9f6655b3b1bd7d88a462e892fe169ca1f
norbek <lightnight1223@gmail.com>
2026-01-12 14:18:19 +0500

    add auth and user routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from app.routers import users, auth
+
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from app.core.limiter import limiter
 
 app = FastAPI()
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
+# Connect the routers we just made
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
+
 @app.get("/")
-def root():
-    return {"message": "Ok"}
+def health_check():
+    return {"status": "ok"}
